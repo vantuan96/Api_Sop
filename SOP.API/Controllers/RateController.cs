7df534159ca10419b0865f1450cf66a3005ead01
vantuan96 <vantuan19961997@gmail.com>
2021-12-12 22:31:14 -0800

    up
    update

--- SOP.API/Controllers/RateController.cs
+++ SOP.API/Controllers/RateController.cs
@@ -1,8 +1,11 @@
 ﻿using SOP.API.Filter;
+using SOP.API.LibaryHelper;
+using SOP.API.Models;
 using SOP.API.Service;
 using SOP.Shared.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -32,6 +35,33 @@ namespace SOP.API.Controllers
                 return result = new MessageReport(false, "Lỗi");
             }
         }
+        [Route("listuser")]
+        [HttpGet]
+        public List<User_SOP> GetListUsers(List<int>  userids)
+        {
+            var lst = new List<User_SOP>();
+            
+            // lấy datatbale user 
+            DataTable dt = UserService.GetDtUser();
+            // Convert to list
+            if (dt != null)
+            {
+                var lstObj = SqlHelper.ConvertTo<User_SOP>(dt);
+                foreach (var item in lstObj)
+                {
+                    foreach (var item1 in userids)
+                    {
+                        if (item.User_Id == item1)
+                        {
+                            lst.Add(item);
+                        }
+                    }
 
+
+                }
+            }
+           
+            return lst;
+        }
     }
 }
